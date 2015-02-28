45642b18d47e0f28e90143c1b914374df72802cd
twhitburn <twhitburn@wisc.edu>
2015-02-28 15:28:26 -0600

    Implemented DLinkedList add and created Test class

--- src/DLinkedList.java
+++ src/DLinkedList.java
@@ -1,46 +1,95 @@
 
-public class DLinkedList<Listnode> implements ListADT<Listnode> {
+public class DLinkedList<E> implements ListADT<E> {
+
+
+	private Listnode<E> head;
+	private Listnode<E> tail;
+	private int numItems;
+
+	public DLinkedList() {
+		head = null;
+		tail = null;
+		numItems = 0;
+	}
+
 
 	@Override
-	public void add(Listnode item) {
-		// TODO Auto-generated method stub
+	public void add(E item) {
+
+		if (item == null) throw new IllegalArgumentException();
+
+		Listnode<E> newnode = new Listnode<E>(item);
+
+		//Special case: empty list
+		if (head == null) {
+			head = newnode;
+			tail = newnode;
+		}
 		
+		//General case:
+		else {
+			tail.setNext(newnode);
+			newnode.setPrev(tail);
+			tail = newnode;
+		}
+		
+		numItems++;
+
 	}
 
 	@Override
-	public void add(int pos, Listnode item) {
-		// TODO Auto-generated method stub
-		
+	public void add(int pos, E item) {
+		if ((item == null) || (pos < 0) || (pos > numItems-1)) {
+			throw new IllegalArgumentException();
+		}
+
+
+		// TODO 
+
 	}
 
 	@Override
-	public boolean contains(Listnode item) {
-		// TODO Auto-generated method stub
+	public boolean contains(E item) {
+		if (item == null) throw new IllegalArgumentException();
 		return false;
+		//TODO
+
+
 	}
 
 	@Override
-	public Listnode get(int pos) {
-		// TODO Auto-generated method stub
+	public E get(int pos) {
+
+		if ((pos < 0) || (pos > numItems-1)) throw new IllegalArgumentException();
+
+		// TODO 
 		return null;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+
+		if (numItems == 0) return true;
+		else return false;
+
 	}
 
 	@Override
-	public Listnode remove(int pos) {
-		// TODO Auto-generated method stub
+	public E remove(int pos) {
+
+		if ((pos < 0) || (pos > numItems-1)) throw new IllegalArgumentException();
+
+
+		// TODO 
 		return null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return numItems;
+
 	}
+
 
 }
---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		DLinkedList<Integer> list = new DLinkedList<Integer>();
+		
+		if (list.isEmpty()) System.out.println("Empty");
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		if (!list.isEmpty()) System.out.println("Not Empty");
+	int i = 0;
+	}
+	
+
+}
